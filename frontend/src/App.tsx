cc545866f73628a281caa11822d823153dd34be0
bijaypokhrel05 <077bei011@ioepc.edu.np>
2026-03-02 21:58:53 +0545

    chore: intialize the tailwind css configuration and install dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <h1>Hello World. This is my new portfolio site!</h1>
+    <h1 className="text-3xl font-bold text-red-500 text-center p-4">Hello World. This is my new portfolio site!</h1>
   );
 };
 
